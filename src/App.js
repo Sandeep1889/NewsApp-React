6cc356522df2401dcd45a9a7dcb5c4a110b02f5e
Sandeep <sandeepkumarg1889@gmail.com>
2024-04-11 10:18:39 +0530

    change into function base component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
+import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import React from "react";
 import News from "./Components/News";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function App() {
+const App = () => {
+  const pageSize = 9;
+
   return (
     <div>
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<News key = "" pageSize={10} country="in"  />} />
-          <Route exact path="/science" element={<News key = "science"pageSize={10} country="in" category="science" />} />
-          <Route exact path="/entertainment" element={<News key = "entertainment"pageSize={10} country="in" category="entertainment" />} />
-          <Route exact path="/health" element={<News key = "health"pageSize={10} country="in" category="health" />} />
-          <Route exact path="/sports" element={<News key = "sports"pageSize={10} country="in" category="sports" />} />
-          <Route exact path="/business" element={<News key = "business"pageSize={10} country="in" category="business" />} />
-          <Route exact path="/technology" element={<News key = "technology"pageSize={10} country="in" category="technology" />} />
-          
+          <Route
+            exact
+            path="/"
+            element={<News key="" pageSize={pageSize} country="in" />}
+          />
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                key="science"
+                pageSize={pageSize}
+                country="in"
+                category="science"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                key="entertainment"
+                pageSize={pageSize}
+                country="in"
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                key="health"
+                pageSize={pageSize}
+                country="in"
+                category="health"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                key="sports"
+                pageSize={pageSize}
+                country="in"
+                category="sports"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                key="business"
+                pageSize={pageSize}
+                country="in"
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                key="technology"
+                pageSize={pageSize}
+                country="in"
+                category="technology"
+              />
+            }
+          />
         </Routes>
       </Router>
     </div>
   );
-}
+};
+
+export default App;
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,133 +1,94 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import NewsItems from "./NewsItems";
 import PropTypes from "prop-types";
-import Spinner from './Spinner.js';
+import Spinner from "./Spinner.js";
+import InfiniteScroll from "react-infinite-scroll-component";
 
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState();
 
-export default class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  // Constructor
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0, // Add totalResults in state
-    };
-  }
-
-
-
-
-  // ComponentDidMount method for fetch the  data from Api
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=18cee3466b9b44ea857959ae61b257b5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+  const UpdateNews = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=18cee3466b9b44ea857959ae61b257b5&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles , totalResults: parsedData.totalResults,loading:false});
-    console.log(parsedData)
-  }
-
-  handlePrevClick = async () => {
-    if (this.state.page > 1) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=18cee3466b9b44ea857959ae61b257b5&page=${
-        this.state.page - 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true})
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        articles: parsedData.articles,
-        page: this.state.page - 1,
-        loading: false,
-      });
-    }
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   };
 
-  handleNextClick = async () => {
-    if (
-      this.state.page + 1 <=
-      Math.ceil(this.state.totalResults / this.props.pageSize)
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=18cee3466b9b44ea857959ae61b257b5&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true})
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        articles: parsedData.articles,
-        page: this.state.page + 1,
-        loading: false,
-      });
-    }
+  useEffect(() => {
+    UpdateNews();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=18cee3466b9b44ea857959ae61b257b5&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
 
-  render() {
-    return (
-      <>
-        <div className="container my-5">
-          <h1 className="text-center">NewsMonkey - Headlines</h1>
-          {this.state.loding && <Spinner/>}
+  return (
+    <>
+      <h1
+        className="text-center"
+        style={{ marginTop: "70px", marginBottom: "30px" }}
+      >
+        NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
           <div className="row">
-            {this.state.articles.map((element) => (
+            {articles.map((element) => (
               <div className="col-md-4" key={element.url}>
                 <NewsItems
                   title={element.title}
                   description={element.description}
                   urlToImage={element.urlToImage}
                   url={element.url}
-                  author ={element.author}
-                  date ={element.publishedAt}
-                  source = {element.source.name}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
                 />
               </div>
             ))}
           </div>
-          <div className="container  d-flex justify-content-between">
-            <button
-              disabled={this.state.page <= 1}
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handlePrevClick}
-            >
-              &laquo; Previous
-            </button>
-            <button
-              disabled={
-                this.state.page >=
-                Math.ceil(this.state.totalResults / this.props.pageSize)
-              }
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handleNextClick}
-            >
-              Next &raquo;
-            </button>
-          </div>
         </div>
-      </>
-    );
-  }
-}
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+
+export default News;
